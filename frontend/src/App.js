61e620066a59ad8ebe58a366042dd42698cba9e3
Nandhagopalan107 <nandhagopalan107@gmail.com>
2024-03-05 12:50:12 +0530

    Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import kid_banner from './Components/Assets/banner_kids.png'
 import Login from './Pages/Login';
 import React from 'react';
 import ManageProducts from './Pages/ManageProducts';
+import AddProduct from './Pages/AddProduct';
  
  
  
@@ -30,6 +31,7 @@ function App() {
           <Route path='/Kids' element={<ShopCategory banner ={kid_banner} category = "kid"/>}/>
           <Route path='/product' element={<Product/>}/>
           <Route path='/manageproducts' element = {<ManageProducts/>}/>
+          <Route path='/addproducts' element = {<AddProduct/>}/>
             <Route path='/product/:id' element = {<Product/>}/>
             <Route path='/cart' element = {<Cart/>}/>
             <Route path='/signup' element = {<LoginSignup/>}/>
