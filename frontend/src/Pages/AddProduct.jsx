99c8c6bd63cba886155bce7555966d7b05b08568
Nandhagopalan107 <nandhagopalan107@gmail.com>
2024-03-05 12:40:27 +0530

    Added css

--- frontend/src/Pages/AddProduct.jsx
+++ frontend/src/Pages/AddProduct.jsx
@@ -8,24 +8,26 @@ const AddProduct = () => {
             Add Products
         </h1>
         <div className="addproduct-fields">
-            <input type="text" name="name" />
-            <input type="text" name="desc" />
+        <label className="label">Product Name</label>
+            <input type="text" name="name" /><br></br>
+            <label className="label">Product Description</label>
+            <input type="text" name="desc" /><br></br>
             <label className="label">Product category</label>
             <select id="category" name="category">
             <option value="" disabled>
               Select a category
             </option>
-
             </select>
+            <br></br>
             <label className="label">Product Image</label>
-          <input type="file" accept=".jpeg, .png" />
+          <input type="file" accept=".jpeg, .png" /><br></br>
           <label className="label">Product quantity</label>
-          <input type="text" name="quantity"/>
+          <input type="text" name="quantity"/><br></br>
           <label className="label">Product price</label>
-          <input type="text"  name="price"/>
+          <input type="text"  name="price"/><br></br>
           <button type="submit" className="add">
             Add product
-          </button>
+          </button><br></br>
         </div>
         </form>
     </div>
