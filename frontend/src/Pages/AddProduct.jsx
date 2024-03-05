9fc8d6cab3b0508bf2f355a04bb080c544456b32
Nandhagopalan107 <nandhagopalan107@gmail.com>
2024-03-05 13:14:56 +0530

    Added css

--- frontend/src/Pages/AddProduct.jsx
+++ frontend/src/Pages/AddProduct.jsx
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 import React from 'react'
 import './CSS/AddProduct.css'
-=======
-import React from "react";
-import { useState } from "react";
-import axios from "axios";
->>>>>>> b948cf41a5d28ae927ee2914d9c2cd5bc114bc84
-
+import axios from 'axios';
+import { useState } from 'react';
 const AddProduct = () => {
   const [selectedFile, setSelectedFile] = useState(null);
 
@@ -47,46 +42,31 @@ const AddProduct = () => {
   return (
     <div className="addproduct">
       <form className="addproduct-form">
-        <h1 className="addproduct-heading">Add Products</h1>
-        <div className="addproduct-fields">
-<<<<<<< HEAD
-          <input type="text" name="name" />
-          <input type="text" name="desc" />
-          <label className="label">Product category</label>
-          <select id="category" name="category">
-            <option value="" disabled>
-              Select a category
-            </option>
-          </select>
-          <label className="label">Product Image</label>
-          <input type="file" accept=".jpeg, .png" onChange={handleFileChange} />
-          <label className="label">Product quantity</label>
-          <input type="text" name="quantity" />
-          <label className="label">Product price</label>
-          <input type="text" name="price" />
-          <button type="submit" className="add" onClick={handleFormSubmit}>
-=======
-        <label className="label">Product Name</label>
-            <input type="text" name="name" /><br></br>
-            <label className="label">Product Description</label>
-            <input type="text" name="desc" /><br></br>
+        <h1 >Add Products</h1>
+        <div className="addproduct-container">
+          <div className="addproduct-fields">
+            <label>Product name</label>
+            <input type="text" name="name" />
+            <label>Product Description</label>
+            <input type="text" name="desc" />
             <label className="label">Product category</label>
             <select id="category" name="category">
-            <option value="" disabled>
-              Select a category
-            </option>
+              <option value="" disabled>
+                Select a category
+              </option>
             </select>
-            <br></br>
-            <label className="label">Product Image</label>
-          <input type="file" accept=".jpeg, .png" /><br></br>
-          <label className="label">Product quantity</label>
-          <input type="text" name="quantity"/><br></br>
-          <label className="label">Product price</label>
-          <input type="text"  name="price"/><br></br>
-          <button type="submit" className="add" class="product-submit">
->>>>>>> 2ec9e98dd151a32acdeaefac85dc467a127b72c9
-            Add product
-          </button><br></br>
+            
+                  <label className="label">Product Image</label>
+                  <input type="file" accept=".jpeg, .png" onChange={handleFileChange} />
+                  <label className="label">Product quantity</label>
+                  <input type="text" name="quantity" />
+            
+            <label className="label">Product price</label>
+            <input type="text" name="price" />
+            <button type="submit" className="add" onClick={handleFormSubmit}>
+              Add product
+            </button><br></br>
+          </div>
         </div>
       </form>
     </div>
