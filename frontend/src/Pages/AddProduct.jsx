aa229530e1202c01f31772eb424eb613208ade91
Nandhagopalan107 <nandhagopalan107@gmail.com>
2024-03-05 16:53:08 +0530

    change

--- frontend/src/Pages/AddProduct.jsx
+++ frontend/src/Pages/AddProduct.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './CSS/AddProduct.css'
 import axios from 'axios';
+import ex_img from '../Components/Assets/product_21.png'
 import { useState } from 'react';
+import { ShopContext } from '../Context/ShopContext';
 const AddProduct = () => {
+  const { all_product} = useContext(ShopContext);
   const [productData, setProductData] = useState({
+    id: 37,
     name: "",
     desc: "",
     category: "",
-    image: null,
+    image: '' ,
     quantity: "",
     price: "",
+    new_price: 50.0,
+    old_price: 80.5,
   });
 
   const handleFileChange = (event) => {
@@ -35,7 +41,6 @@ const AddProduct = () => {
       formData.append("productName", productData.name);
       formData.append("description", productData.desc);
       formData.append("category", productData.category);
-      formData.append("filepath", productData.image);
       formData.append("quantity", productData.quantity);
       formData.append("price", productData.price);
       for (const [key, value] of formData.entries()) {
@@ -43,16 +48,20 @@ const AddProduct = () => {
     }
 
       // // Adjust the URL according to your backend endpoint
-      const response = await axios.post(
-        "http://localhost:8080/addnewproduct",
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
+      // const response = await axios.post(
+      //   "http://localhost:8080/addnewproduct",
+      //   formData,
+      //   {
+      //     headers: {
+      //       "Content-Type": "multipart/form-data",
+      //     },
+      //   }
+      // );
 
+      all_product.push(productData);
+      console.log(all_product)
+
+      
       // console.log(response.data); // Handle the response as needed
     } catch (error) {
       console.error("Error adding product:", error.message);
@@ -72,13 +81,11 @@ const AddProduct = () => {
             <option value="" disabled>
               Select a category
             </option>
-            <option>Men</option>
-            <option>Women</option>
-            <option>Kids</option>
+            <option>men</option>
+            <option>women</option>
+            <option>kids</option>
 
           </select>
-          <label className="label">Product Image</label>
-          <input type="file" accept=".jpeg, .png" onChange={handleFileChange} />
           <label className="label">Product quantity</label>
           <input type="number" name="quantity" onChange={handleInputChange}/>
           <label className="label">Product price</label>
